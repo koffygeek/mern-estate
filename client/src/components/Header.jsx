71df816c0fb86283889386686b6e3fb6a80a797f
Koffi GREBE <yvan.grebe@email.com>
2024-04-15 14:37:17 +0100

    added the header component and updated the routes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 
 const Header = () => {
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-          <span className="text-slate-500">CodingCleverly</span>
-          <span className="text-sl">Estate</span>
-        </h1>
-        <form>
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">CodingCleverly</span>
+            <span className="text-sl">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="Search..."
@@ -17,6 +20,15 @@ const Header = () => {
           />
           <FaSearch className="text-slate-600" />
         </form>
+        <ul className="flex gap-4">
+          <li className="hidden sm:inline text-slate-700 hover:underline">
+            Home
+          </li>
+          <li className="hidden sm:inline text-slate-700 hover:underline">
+            About
+          </li>
+          <li className="text-slate-700 hover:underline">Sign In</li>
+        </ul>
       </div>
     </header>
   );
