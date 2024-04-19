339825d9efb29cae4482081fe14cec7124b6abb2
Koffi GREBE <yvan.grebe@email.com>
2024-04-19 17:35:12 +0100

    error mongooseerror

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,13 +21,19 @@ const Header = () => {
           <FaSearch className="text-slate-600" />
         </form>
         <ul className="flex gap-4">
-          <li className="hidden sm:inline text-slate-700 hover:underline">
-            Home
-          </li>
-          <li className="hidden sm:inline text-slate-700 hover:underline">
-            About
-          </li>
-          <li className="text-slate-700 hover:underline">Sign In</li>
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="text-slate-700 hover:underline">Sign In</li>
+          </Link>
         </ul>
       </div>
     </header>
